8b117f14a3ea6ed82463643475937d884a29a420
Aaron Sun <asunaron@umich.edu>
2026-03-28 13:44:15 -0400

    Testing Deployment

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,12 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from routes.health import router
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["*"],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
@@ -15,6 +15,3 @@ app.add_middleware(
 @app.get("/")
 def root():
     return {"app": "clerse", "status": "running"}
-
-
-app.include_router(router)
